5bbc8218610979c727a32132896f33260f635abc
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2021-02-14 14:24:45 +0000

    ainput wrapper for chapter 3

---
+++ DRL_Hands_On/Chapter_3_DeepLearning/atari_gan.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import random
+import argparse
+import cv2
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tensorboardX import SummaryWriter
+
+import torchvision.utils as vutils
+
+import gym
+import gym.spaces
+
+import numpy as np
+
+log = gym.logger
+log.set_level(gym.logger.INFO)
+
+LATENT_VECTOR_SIZE = 100
+DISCR_FILTERS = 64
+GENER_FILTERS = 64
+BATCH_SIZE = 16
+
+# dimension input image will be rescaled
+IMAGE_SIZE = 64
+
+LEARNING_RATE = 0.0001
+REPORT_EVERY_ITER = 100
+SAVE_IMAGE_EVERY_ITER = 1000
+
+class InputWrapper(gym.ObservationWrapper):
+    """
+    Preprocessing of input numpy array
+    1. resize image into predefined size
+    2. move color channel axis to a first place
+    """
+    def __init__(self, *args):
+        super(InputWrapper, self).__init__(*args)
+        assert isinstance(self.observation_space, gym.spaces.Box)
+        self.observation_space = gym.spaces.Box(self.observation(old_space.low), self.observation(old_space.high), dtype=np.float32)
+
+    def observation(self, observation):
+        # resize image
+        new_obs = cv2.resize(observation, (IMAGE_SIZE, IMAGE_SIZE))
+        # transform channel to 1st dim
+        new_obs = np.moveaxis(new_obs, 2, 0)
+        return new_obs.atype(np.float32)
+
+
